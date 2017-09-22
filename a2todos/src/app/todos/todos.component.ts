ea4e0d29bca1dceefc38e3647520ca2a7d2aac61
OstapHetman <prickly4528@gmail.com>
2017-09-22 12:24:46 +0300

    implement adding and deleting todos

--- a2todos/src/app/todos/todos.component.ts
+++ a2todos/src/app/todos/todos.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  todos;
+  text;
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        text: 'Pickup kids at school'
+      },
+      {
+        text:'Meeting with boss'
+      },
+      {
+        text: 'Dinner with wife'
+      }
+    ];
+  }
+  addTodo() {
+    this.todos.push({
+      text: this.text
+    });
+  }
+  deleteTodo(todoText) {
+  for(var i=0; i < this.todos.length; i++) {
+      if(this.todos[i].text == todoText) {
+        this.todos.splice(i,1);
+      }  
+    }
   }
 
 }
